ac572c46c246f19d8e6597fb67771cf61b110416
PyromancerBoom <priyanshmagic@gmail.com>
2024-01-09 12:29:17 +0530

    bug fix

--- backend/eligibility.py
+++ backend/eligibility.py
@@ -2,10 +2,6 @@ from pydantic import BaseModel
 from fastapi import APIRouter, Body
 
 
-router = APIRouter()
-
-
-@router.post("/eligible")
 async def eligible() -> bool:
     # TODO: Implement eligibility check
     is_eligible = True
