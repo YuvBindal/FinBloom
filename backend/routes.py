2c126c600b313c46e7cf260beb1b11b37537806b
Shayaan Sultan <sultan.shayaan@gmail.com>
2024-01-10 18:49:33 +0500

    Converted int to str

--- backend/routes.py
+++ backend/routes.py
@@ -10,18 +10,18 @@ from eligibility import eligible
 class LoanData(BaseModel):
     # Define the structure of your data here
     # For example:
-    loan_id: int
-    no_of_dependents: int
-    education: int
-    self_employed: int
-    income_annum: int
-    loan_amount: int
-    loan_term: int
-    cibil_score: int
-    residential_assets_value: int
-    commercial_assets_value: int
-    luxury_assets_value: int
-    bank_asset_value: int
+    loan_id: str
+    no_of_dependents: str
+    education: str
+    self_employed: str
+    income_annum: str
+    loan_amount: str
+    loan_term: str
+    cibil_score: str
+    residential_assets_value: str
+    commercial_assets_value: str
+    luxury_assets_value: str
+    bank_asset_value: str
 
 
 router = APIRouter()
