6cae9027ff55d8a333a052e6072e31e4b199d981
PyromancerBoom <priyanshmagic@gmail.com>
2024-01-15 21:45:47 +0800

    update layout

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -6,14 +6,19 @@ import { AuthContextProvider } from "./utils/auth-helper";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children, className }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthContextProvider>
-          <Navbar />
-          {children}
-        </AuthContextProvider>
+        <div
+          className={`w-full h-full max-w-1100 inline-block bg-dark py-0 px-64 pt-0
+          ${className}`}
+        >
+          <AuthContextProvider>
+            <Navbar />
+            {children}
+          </AuthContextProvider>
+        </div>
       </body>
     </html>
   );
