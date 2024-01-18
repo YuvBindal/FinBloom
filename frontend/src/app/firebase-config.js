4270b1da03e0701131e934a3898cc8b78000cf76
PyromancerBoom <priyanshmagic@gmail.com>
2024-01-18 20:55:51 +0800

    idk

--- frontend/src/app/firebase-config.js
+++ frontend/src/app/firebase-config.js
@@ -1,6 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, setPersistence, browserSessionPersistence  } from "firebase/auth";
+import {
+  getAuth,
+  setPersistence,
+  browserSessionPersistence,
+} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -8,20 +12,22 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCe3A0vLHLsxdK24CWYMVAvxwb_gMT7oDU",
-  authDomain: "bloomfin-8d51a.firebaseapp.com",
-  projectId: "bloomfin-8d51a",
-  storageBucket: "bloomfin-8d51a.appspot.com",
-  messagingSenderId: "852078713180",
-  appId: "1:852078713180:web:29275d57d57e6db06ac999",
-  measurementId: "G-JQZ1PET1Y3"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-setPersistence(auth, browserSessionPersistence)
+setPersistence(auth, browserSessionPersistence);
 
 const db = getFirestore(app);
 export { auth, db };
